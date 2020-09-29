f75dcf56ef97a9b9025d8eab3b1c9b1dd82466e6
sazzad4623 <sazzad111111@gmail.com>
2020-09-29 13:12:54 +0600

    chat bot

--- src/App.js
+++ src/App.js
@@ -1,16 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./components/navbar";
 import Footer from "./components/footer";
 import Routing from "./Routing";
+import Chat from "./components/chat";
+import "./chat.css";
 import { BrowserRouter } from "react-router-dom";
 
 function App() {
+  let [showChat, setShowChat] = useState(false);
+
+  const startChat = () => {
+    setShowChat(true);
+  };
+  const hideChat = () => {
+    setShowChat(false);
+  };
   return (
     <BrowserRouter>
-      <Navbar></Navbar>
-      <Routing></Routing>
-      <div></div>
-      <Footer></Footer>
+      <div>
+        <Navbar></Navbar>
+        <Routing></Routing>
+
+        <Footer></Footer>
+        <div className="bot">
+          <div style={{ display: showChat ? "" : "none" }}>
+            <Chat />
+          </div>
+          {/* <div> {showChat ? <SimpleForm></SimpleForm> : null} </div> */}
+          <div>
+            {!showChat ? (
+              <button className="btn btn-info" onClick={() => startChat()}>
+                click to chat...{" "}
+              </button>
+            ) : (
+              <button className="btn btn-info" onClick={() => hideChat()}>
+                click to hide...{" "}
+              </button>
+            )}
+          </div>
+        </div>
+      </div>
     </BrowserRouter>
   );
 }
